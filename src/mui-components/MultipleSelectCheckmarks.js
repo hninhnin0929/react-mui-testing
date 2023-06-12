fdce660474942fb72a384efc0f0b3824d9ee9267
hninhnin0929 <hninhnin0929@gmail.com>
2023-06-12 17:53:57 +0900

    multipleselectcheckmarks

---
+++ src/mui-components/MultipleSelectCheckmarks.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import ListItemText from '@mui/material/ListItemText';
+import Select from '@mui/material/Select';
+import Checkbox from '@mui/material/Checkbox';
+import { event } from 'jquery';
+
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const names = [
+  'Oliver Hansen',
+  'Van Henry',
+  'April Tucker',
+  'Ralph Hubbard',
+  'Omar Alexander',
+  'Carlos Abbott',
+  'Miriam Wagner',
+  'Bradley Wilkerson',
+  'Virginia Andrews',
+  'Kelly Snyder',
+];
+
+export default function MultipleSelectCheckmarks(){
+
+    const [personName, setPersonName] = React.useState([]);
+
+    const handleChange = (event) => {
+        
+    };
+}
